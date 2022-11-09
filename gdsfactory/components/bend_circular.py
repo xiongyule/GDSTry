e1ce91406d2b06900ba9b0ecfe0b2ddd2098cc09
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-08 21:12:32 -0800

    remove absorb

--- gdsfactory/components/bend_circular.py
+++ gdsfactory/components/bend_circular.py
@@ -41,7 +41,7 @@ def bend_circular(
     ref = c << path
     c.add_ports(ref.ports)
 
-    # c.absorb(ref)
+    c.absorb(ref)
     c.info["length"] = float(snap_to_grid(p.length()))
     c.info["dy"] = snap_to_grid(float(abs(p.points[0][0] - p.points[-1][0])))
     c.info["radius"] = float(radius)
